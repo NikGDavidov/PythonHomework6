f34d38bf502ebfaac30dacdc2b00423f7c93240d
Nikolay_Davydov <skmoscow74@yandex.ru>
2022-07-29 23:14:11 +0300

    homework 6_1

--- Task3.py
+++ Task3.py
@@ -10,5 +10,6 @@ en = enumerate(list1)
 # print(en)
 res = list(filter(lambda x:x[0]%2 == 0, en)) #предполагаем что позиция 1 - нечетная, соответствует индексу 0 (четному)
 res = list(map(lambda x:x[1], res ))
+result = sum(res)
 
-print (f'{list1} -> {res}')
+print (f'{list1} -> {result}')
